9dcee7d492360fe98819c37c63dd0010f6d33499
Michel Rijnders <mies@tty.nl>
2012-04-28 13:19:52 +0200

    Rename unused variables

--- apps/dk_ifi/src/dk_ifi.erl
+++ apps/dk_ifi/src/dk_ifi.erl
@@ -2,25 +2,25 @@
 -export([add_dom/1, del_dom/1, add_cat/2, del_cat/2]).
 -export([add_pointer/4, del_pointer/4, get_inv_list/3]).
 
-add_dom(DomId) ->
+add_dom(_DomId) ->
     ok.
 
-del_dom(DomId) ->
+del_dom(_DomId) ->
     ok.
 
-add_cat(DomId, CatId) ->
+add_cat(_DomId, _CatId) ->
     ok.
 
-del_cat(DomId, CatId)
+del_cat(_DomId, _CatId) ->
     ok.
 
-add_pointer(DomId, CatId, DocId, Term) ->
+add_pointer(_DomId, _CatId, _DocId, _Term) ->
     ok.
 
-del_pointer(DomId, CatId, DocId, Term) ->
+del_pointer(_DomId, _CatId, _DocId, _Term) ->
     ok.
 
-get_inv_list(DomId, CatId, Term) ->
+get_inv_list(_DomId, _CatId, _Term) ->
     [].
 
 %%% Local variables:
