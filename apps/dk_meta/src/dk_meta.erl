1d5b0609391ef09d65bb04aef31ab26926b36657
Michel Rijnders <mies@tty.nl>
2012-05-07 12:25:33 +0200

    Add domain language functions

--- apps/dk_meta/src/dk_meta.erl
+++ apps/dk_meta/src/dk_meta.erl
@@ -3,6 +3,7 @@
 -behaviour(gen_server).
 
 %% API
+-export([add_dom/2, del_dom/1, dom_lang/1]).
 -export([start_link/0]).
 
 %% gen_server callbacks
@@ -11,11 +12,23 @@
 
 -define(SERVER, ?MODULE).
 
--record(state, {}).
+-record(state, {dom_lang}).
 
 %%----------------------------------------------------------------------------
 %% API
 %%----------------------------------------------------------------------------
+
+%% @doc Adds a domain.
+add_dom(DomId, Lang) ->
+    gen_server:call(?SERVER, {add_dom, DomId, Lang}).
+    
+%% @doc Adds a domain.
+del_dom(DomId) ->
+    gen_server:call(?SERVER, {del_dom, DomId}).
+    
+%% @doc Returns the language of a domain.
+dom_lang(DomId) ->
+    gen_server:call(?SERVER, {dom_lang, DomId}).
 
 start_link() ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
@@ -26,11 +39,24 @@ start_link() ->
 
 %% @private
 init([]) ->
-    {ok, #state{}}.
+    {ok, #state{dom_lang = dict:new()}}.
 
 %% @private
-handle_call(_Request, _From, State) ->
-    Reply = ok,
+handle_call({add_dom, DomId, Lang}, _From, State) ->
+    Dict = State#state.dom_lang,
+    case dict:is_key(DomId, Dict) of
+        true ->
+            {reply, error, State};
+        false ->
+            NextState = State#state{dom_lang = dict:store(DomId, Lang, Dict)},
+            {reply, ok, NextState}
+    end;
+handle_call({del_dom, DomId}, _From, State) ->
+    NextState =
+        State#state{dom_lang = dict:erase(DomId, State#state.dom_lang)},
+    {reply, ok, NextState};
+handle_call({dom_lang, DomId}, _From, State) ->
+    Reply = dict:find(DomId, State#state.dom_lang),
     {reply, Reply, State}.
 
 %% @private
