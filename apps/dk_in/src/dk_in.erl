a7d9727f43e25cdc197d3a78d3176b531799310d
Michel Rijnders <mies@tty.nl>
2012-06-08 09:35:20 +0200

    Use integers for IDs

--- apps/dk_in/src/dk_in.erl
+++ apps/dk_in/src/dk_in.erl
@@ -22,7 +22,8 @@ add_doc(Binary) ->
     [Line, Rest] = binary:split(Binary, <<"\n">>), %% does this work correctly with UTF-8?
     [Id, Text] = binary:split(Line, <<" ">>),
     %% io:format("doc ~p~n", [Id]),
-    plists:foreach(fun (T) -> dk_nii:add_post(T, Id) end, dk_pp:terms(Text, "en")),
+    IntId = list_to_integer(binary_to_list(Id)),
+    plists:foreach(fun (T) -> dk_nii:add_post(T, IntId) end, dk_pp:terms(Text, "en")),
     add_doc(Rest).
 
 %% Local variables:
