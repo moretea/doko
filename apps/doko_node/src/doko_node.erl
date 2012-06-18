83bf171a39fd5e62cf0ebaa7ca152459001106e3
Michel Rijnders <g.a.c.rijnders@gmail.com>
2012-06-18 08:24:21 +0200

    Merge "dk_in" into "doko_node"

--- apps/doko_node/src/doko_node.erl
+++ apps/doko_node/src/doko_node.erl
@@ -2,6 +2,7 @@
 
 %% API
 -export([start/0, start/1, stop/0]).
+-export([load/1]).
 
 %%----------------------------------------------------------------------------
 %% API
@@ -20,6 +21,26 @@ start(Nodes) ->
 %% @doc Stops the node.
 stop() ->
     application:stop(doko_node).
+
+load(File) ->
+    {ok, Binary} = file:read_file(File),
+    add_docs(Binary),
+    ok.
+
+%%----------------------------------------------------------------------------
+%% Internal functions
+%%----------------------------------------------------------------------------
+
+add_docs(<<>>) ->
+    ok;
+add_docs(Binary) ->
+    [Line, Rest] = binary:split(Binary, <<"\n">>), % does this work correctly
+                                                   % with UTF-8?
+    [Id, Text] = binary:split(Line, <<" ">>),
+    IntId = list_to_integer(binary_to_list(Id)),
+    plists:foreach(fun (T) -> doko_index:add_doc_id(T, IntId) end,
+                   dk_pp:terms(Text, "en")),
+    add_docs(Rest).
 
 %% Local variables:
 %% mode: erlang
