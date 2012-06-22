ab183111439213477331910a5862732627250a2f
Michel Rijnders <mies@tty.nl>
2012-06-22 10:30:26 +0200

    Remove commented code

--- apps/doko_node/src/doko_node.erl
+++ apps/doko_node/src/doko_node.erl
@@ -26,26 +26,6 @@ start() ->
 stop() ->
     application:stop(doko_node).
 
-%% load(File) ->
-%%     {ok,Binary} = file:read_file(File),
-%%     add_docs(Binary),
-%%     ok.
-
-%%----------------------------------------------------------------------------
-%% Internal functions
-%%----------------------------------------------------------------------------
-
-%% add_docs(<<>>) ->
-%%     ok;
-%% add_docs(Binary) ->
-%%     [Line,Rest] = binary:split(Binary, <<"\n">>), % does this work correctly
-%%                                                   % with UTF-8?
-%%     [Id,Text] = binary:split(Line, <<" ">>),
-%%     IntId = list_to_integer(binary_to_list(Id)),
-%%     plists:foreach(fun (T) -> doko_index:add_doc_id(T, IntId) end,
-%%                    doko_preprocessing:terms(Text, "en")),
-%%     add_docs(Rest).
-
 %% Local variables:
 %% mode: erlang
 %% fill-column: 78
