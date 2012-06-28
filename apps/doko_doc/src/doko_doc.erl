b70efa9c4036ad50f1b2c5e6975cf1e723ce5fb2
Michel Rijnders <mies@tty.nl>
2012-06-28 13:59:49 +0200

    Extend "doko_doc" module

--- apps/doko_doc/src/doko_doc.erl
+++ apps/doko_doc/src/doko_doc.erl
@@ -1,26 +1,33 @@
 -module(doko_doc).
+-include("../include/doko_doc.hrl").
 
 %% API
--export([new/2]).
+-export([new/2,preprocess/2]).
 -export([id/1,zone/2]).
-
--record(doc, {id, zones}).
+-export([zone_ids/1]).
 
 %%----------------------------------------------------------------------------
 %% API
 %%----------------------------------------------------------------------------
 
+-spec new(doc_id(), nonempty_list(zone())) -> #doc{}.
 new(Id, Zones) ->
-    #doc{id = Id,zones = dict:from_list(Zones)}.
+    #doc{id = Id,zones = Zones}.
+
+preprocess(#doc{id = Id,zones = RawZones,raw = true}, Lang) ->
+    Preprocess = fun (Text) -> doko_preprocessing:uterms(Text, Lang) end,
+    #doc{id    = Id,
+         zones = [{ZoneId,Preprocess(Text)} || {ZoneId,Text} <- RawZones],
+         raw   = false}.
 
 id(#doc{id = Id}) ->
     Id.
 
-zone(ZoneName, #doc{zones = Zones}) ->
-    case dict:find(ZoneName, Zones) of
-        {ok,Value} -> Value;
-        error -> undefined
-    end.
+zone(ZoneId, #doc{zones = Zones}) ->
+    proplists:get_value(ZoneId, Zones, undefined).
+
+zone_ids(#doc{zones = Zones}) ->
+    proplists:get_keys(Zones).
 
 %% Local variables:
 %% mode: erlang
