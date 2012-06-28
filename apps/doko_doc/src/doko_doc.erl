290ce71d226162ef8f2ac4fbf22049f7d879bfd0
Michel Rijnders <mies@tty.nl>
2012-06-28 14:38:46 +0200

    Add function specs

--- apps/doko_doc/src/doko_doc.erl
+++ apps/doko_doc/src/doko_doc.erl
@@ -14,18 +14,22 @@
 new(Id, Zones) ->
     #doc{id = Id,zones = Zones}.
 
+-spec preprocess(#doc{}, iso_639_1()) -> #doc{}.
 preprocess(#doc{id = Id,zones = RawZones,raw = true}, Lang) ->
     Preprocess = fun (Text) -> doko_preprocessing:uterms(Text, Lang) end,
     #doc{id    = Id,
          zones = [{ZoneId,Preprocess(Text)} || {ZoneId,Text} <- RawZones],
          raw   = false}.
 
+-spec id(#doc{}) -> doc_id().
 id(#doc{id = Id}) ->
     Id.
 
+-spec zone(zone_id(), #doc{}) -> utf8_string() | undefined.
 zone(ZoneId, #doc{zones = Zones}) ->
     proplists:get_value(ZoneId, Zones, undefined).
 
+-spec zone_ids(#doc{}) -> nonempty_list(zone_id()).
 zone_ids(#doc{zones = Zones}) ->
     proplists:get_keys(Zones).
 
